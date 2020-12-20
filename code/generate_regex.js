4bd15ed8d68c32b21ba904bc68407a9132c891b4
William Burdett <william.burdett1@gmail.com>
2020-12-20 17:28:20 -0500

    Tagname

---
+++ code/generate_regex.js
@@ -0,0 +1,9 @@
+function get_checked_skills(){
+
+}
+
+function generate_regex(){
+    let source_type = document.getElementById("source_type").value;
+    let damage_type = document.getElementById("damage_type").value;
+    let skills = []
+}
