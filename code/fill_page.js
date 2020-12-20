4d4d297b238bd3e51ad48d3481f80d22f2730993
William Burdett <william.burdett1@gmail.com>
2020-12-19 23:25:24 -0500

    AT THE END OF THE PAGE

--- code/fill_page.js
+++ code/fill_page.js
@@ -1,14 +1,13 @@
-function add_to_select(select_id, value){
-    let select = document.getElementById(select_id);
+function add_to_select(select, value){
     let option = document.createElement("option");
     option.text = value;
     select.add(option);
 }
 
 function fill_values(){
-    
-    direct_damage_types.forEach(damage_type => add_to_select("source damage type", damage_type));
-    dot_damage_types.forEach(damage_type => add_to_select("source damage type", damage_type));
+    let source_damage_type = document.getElementById("source damage type");
+    direct_damage_types.forEach(damage_type => add_to_select(source_damage_type, damage_type));
+    dot_damage_types.forEach(damage_type => add_to_select(source_damage_type, damage_type));
 }
 
 fill_values();
