34f0cfb3c32c43ca9de8644f426c7adc91ba68e9
William Burdett <william.burdett1@gmail.com>
2020-12-19 22:30:42 -0500

    Testing out json

--- code/generate_regex.js
+++ code/generate_regex.js
@@ -1,15 +1,11 @@
-let classes;
 
-fetch("../database/classes.json")
+classes = fetch("../database/classes.json")
   .then(response => response.json())
-  .then(json => classes = json);
 
 console.log(classes)
 
-let skills;
 
-fetch("../database/skills.json")
+skills = fetch("../database/skills.json")
   .then(response => response.json())
-  .then(json => skills = json);
 
 console.log(skills)
