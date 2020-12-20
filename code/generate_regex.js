09c5c796c9ada8eb6147e46a8dfaba778192ee9f
William Burdett <william.burdett1@gmail.com>
2020-12-19 22:28:47 -0500

    Update json to read both classes and skills

--- code/generate_regex.js
+++ code/generate_regex.js
@@ -1,3 +1,15 @@
+let classes;
+
 fetch("../database/classes.json")
   .then(response => response.json())
-  .then(json => console.log(json));
+  .then(json => classes = json);
+
+console.log(classes)
+
+let skills;
+
+fetch("../database/skills.json")
+  .then(response => response.json())
+  .then(json => skills = json);
+
+console.log(skills)
