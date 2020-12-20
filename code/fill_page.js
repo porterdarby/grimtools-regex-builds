c1bf4303a072561a6c16c9ba079f7fa1f43f581a
William Burdett <william.burdett1@gmail.com>
2020-12-19 23:32:19 -0500

    Procastinating sleep through code

--- code/fill_page.js
+++ code/fill_page.js
@@ -8,6 +8,10 @@ function fill_values(){
     let source_damage_type = document.getElementById("source damage type");
     direct_damage_types.forEach(damage_type => add_to_select(source_damage_type, damage_type));
     dot_damage_types.forEach(damage_type => add_to_select(source_damage_type, damage_type));
+
+    let class_1 = document.getElementById("class 1");
+    Object.entries(classes).forEach(([key, value]) => add_to_select(class_1, key))
+    Object.entries(classes).forEach(([key, value]) => add_to_select(class_2, key))
 }
 
 fill_values();
