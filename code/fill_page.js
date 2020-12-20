00e70469a9e11f67c1f1be8e5008528583e29725
William Burdett <william.burdett1@gmail.com>
2020-12-20 13:41:57 -0500

    Added list of class skills

--- code/fill_page.js
+++ code/fill_page.js
@@ -1,6 +1,7 @@
 function add_to_select(select, value){
     let option = document.createElement("option");
     option.text = value;
+    option.value = value;
     select.add(option);
 }
 
--- database/skills.js
+++ database/skills.js
@@ -1,97 +1,167 @@
-const skills = {
-    "savagery": [
-        "tenacity of the board",
-        "storm touched"
-    ],
-    "mogdrogen's pact": [
-        "heart of the wild",
-        "oak skin"
-    ],
-    "field command": [
-        "squad tactics"
-    ],
-    "summon familiar": [
-        "mend flesh",
-        "storm spirit",
-        "lightning strike"
-    ],
-    "raise skeletons": [
-        "undead legion",
-        "will of the crypt"
-    ],
-    "spectral binding": [
-        "spectral wrath"
-    ],
-    "curse of frailty": [
-        "vulnerability"
-    ],
-    "blood of dreeg": [
-        "aspect of the guardian"
-    ],
-    "summon hellhound": [
-        "ember claw",
-        "hellfire",
-        "infernal breath"
-    ],
-    "summon blight fiend": [
-        "rotting fumes",
-        "blight burst"
-    ],
-    "bonds of bysmiel": [
-        "manipulation"
-    ],
-    "summon briarthorn": [
-        "emboldening presence",
-        "ground slam"
-    ],
-    "wendigo totem": [
-        "blood pact"
-    ],
-    "grasping vines": [
-        "entangling vines"
-    ],
-    "veil of shadow": [
-        "night's chill"
-    ],
-    "pneumatic burst": [
-        "shadow dance",
-        "elemental awakening"
-    ],
-    "blitz": [
-        "blindside"
-    ],
-    "maiven's sphere of protection": [
-        "conversion"
-    ],
-    "mortar trap": [
-        "heavy ordnance",
-        "the big one"
-    ],
-    "canister bomb": [
-        "improved casing",
-        "ulzuin's chosen"
-    ],
-    "grenado": [
-        "high impact",
-        "shattering blast",
-        "ulzuin's chosen"
-    ],
-    "stun jacks": [
-        "full spread",
-        "ulzuin's chosen"
-    ],
-    "phantasmal blades": [
-        "heart seeker",
-        "nether edge"
-    ],
-    "thermite mine": [
-        "hellfire mine"
-    ],
-    "vire's might": [
-        "volcanic stride",
-        "tectonic shift"
-    ],
-    "callidor's tempest": [
-        "inferno"
-    ]
-};
+const skills = [
+    {
+        "class_name": "soldier",
+        "class_skills": [
+            {"name": "Blade Arc", "skills": [{"name": "Clean Sweep"}, {"name": "Laceration"}]},
+            {"name": "Blitz", "skills": [{"name": "Blindside"}]},
+            {"name": "Cadence", "skills": [{"name": "Fighting Form"}, {"name": "Deadly Momentum"}]},
+            {"name": "Forcewave", "skills": [{"name": "Rending Force"}, {"name": "Internal Trauma"}]},
+            {"name": "Overguard", "skills": []},
+            {"name": "War Cry" [{"name": "Break Morale"}]},
+            {"name": "Decorated Soldier", "skills": []},
+            {"name": "Fighting Spirit", "skills": []},
+            {"name": "Markovian's Advantage", "skills": []},
+            {"name": "Menhir's Will", "skills": []},
+            {"name": "Military Conditioning", "skills": []},
+            {"name": "Scars of Battle", "skills": []},
+            {"name": "Shield Training", "skills": []},
+            {"name": "Veterancy", "skills": []},
+            {"name": "Zolhan's Technique", "skills": []},
+            {"name": "Counter Strike", "skills": []},
+            {"name": "Menhir's Bulwark", "skills": []},
+            {"name": "Oleron's Rage", "skills": []},
+            {"name": "Field Command", "skills": [{"name": "Squad Tactics"}]}
+        ]
+    },
+    {
+        "class_name": "demolitionist",
+        "class_skills": [
+            {"name": "Blackwater Cocktail", "skills": [{"name": "High Potency"}, {"name": "Demon Fire"}, {"name": "Agonizing Flames"}]},
+            {"name": "Canister Bomb", "skills": [{"name": "Concussive Bomb"}, {"name": "Improved Casing"}]},
+            {"name": "Fire Strike", "skills": [{"name": "Explosive Strike"}, {"name": "Static Strike"}, {"name": "Brimstone"}]},
+            {"name": "Flashbang", "skills": [{"name": "Searing Light"}]},
+            {"name": "Grenado", "skills": [{"name": "High Impact"}, {"name": "Shattering Blast"}]},
+            {"name": "Mortar Trap", "skills": [{"name": "Heavy Ordnance"}, {"name": "\"The Big One\""}]},
+            {"name": "Stun Jacks", "skills": [{"name": "Quick Jacks"}, {"name": "Full Spread"}]},
+            {"name": "Thermite Mine", "skills": [{"name": "Hellfire Mine"}]},
+            {"name": "Blast Shield", "skills": []},
+            {"name": "Ulzuin's Chosen", "skills": []},
+            {"name": "Flamed Touched", "skills": [{"name": "Temper"}]},
+            {"name": "Vindicative Flame", "skills": [{"name": "Ulzuin's Wrath"}]}
+        ]
+    }, {
+        "class_name": "occultist",
+        "class_skills": [
+            {"name": "Blood of Dreeg", "skills": [{"name": "Aspect of the Guardian"}]},
+            {"name": "Blood Pox", "skills": [{"name": "Fevered Rage"}, {"name": "Wasting"}, {"name": "Black Death"}]},
+            {"name": "Curse of Frailty", "skills": [{"name": "Vulnerability"}]},
+            {"name": "Doom Bolt", "skills": []},
+            {"name": "Dreeg's Evil Eye", "skills": [{"name": "Blood Burst"}, {"name": "Focused Gaze"}, {"name": "Terrifying Gaze"}, {"name": "Vile Eruption"}]},
+            {"name": "Sigil of Consumption", "skills": [{"name": "Destruction"}]},
+            {"name": "Bonds of Bysmiel", "skills": [{"name": "Manipulation"}]},
+            {"name": "Possession", "skills": []},
+            {"name": "Solael's Witchfire", "skills": [{"name": "Consecrated Blade"}, {"name": "Second Rite"}]},
+            {"name": "Summon Familiar", "skills": [{"name": "Mend Flesh"}, {"name": "Storm Spirit"}, {"name": "Lightning Strike"}]},
+            {"name": "Summon Hellhound", "skills": [{"name": "Ember Claw"}, {"name": "Hellfire"}, {"name": "Infernal Breath"}]}
+        ]
+    }, {
+        "class_name": "nightblade",
+        "class_skills": [
+            {"name": "Amarasta's Blade Burst", "skills": [{"name": "Lethal Assault"}]},
+            {"name": "Blade Barrier, Blade Trap", "skills": [{"name": "Devouring Blades"}]},
+            {"name": "Phantasmal Blades", "skills": [{"name": "Frenetic Throw"}, {"name": "Heart Seeker"}, {"name": "Nether Edge"}]},
+            {"name": "Pneumatic Burst", "skills": [{"name": "Breath of Belgothian"}, {"name": "Shadow Dance"}, {"name": "Elemental Awakening"}]},
+            {"name": "Ring of Steel", "skills": [{"name": "Ring of Frost"}, {"name": "Circle of Slaughter"}]},
+            {"name": "Shadow Strike", "skills": [{"name": "Nidalla's Justifiable Ends"}, {"name": "Nightfall"}]},
+            {"name": "Anatomy of Murder", "skills": []},
+            {"name": "Dual Blades", "skills": [{"name": "Belgothian's Shears"}, {"name": "Hidden Hands"}, {"name": "Amarasta's Quick Cut"}, {"name": "Whirling Death"}, {"name": "Execution"}]},
+            {"name": "Merciless Repertoire", "skills": []},
+            {"name": "Phantasmal Armor", "skills": []},
+            {"name": "Veil of Shadow", "skills": [{"name": "Night's Chill"}]},
+            {"name": "Blade Spirit", "skills": []}
+        ]
+    }, {
+        "class_name": "aranist",
+        "class_skills": [
+            {"name": "Albrecht's Aether Ray", "skills": [{"name": "Tainted Power"}, {"name": "Disintegration"}]},
+            {"name": "Callidor's Tempest", "skills": [{"name": "Wrath of Agrivix"}, {"name": "Inferno"}]},
+            {"name": "Devastation", "skills": []},
+            {"name": "Maiven's Sphere of Protection", "skills": [{"name": "Conversion"}]},
+            {"name": "Mirror of Ereoctes, Nullification, Olexra's Flash Freeze", "skills": [{"name": "Absolute Zero"}]},
+            {"name": "Panetti's Replicating Missile", "skills": [{"name": "Distortion"}, {"name": "Supercharged"}, {"name": "Proliferation"}]},
+            {"name": "Trozan's Sky Shard", "skills": [{"name": "Frozen Core"}, {"name": "Shattered Star"}]},
+            {"name": "Arcane Will", "skills": []},
+            {"name": "Fabric of Reality", "skills": []},
+            {"name": "Inner Focus", "skills": []},
+            {"name": "Mental Alacrity", "skills": []},
+            {"name": "Iskandra's Elemental Exchange", "skills": [{"name": "Manifestation"}, {"name": "Overload"}, {"name": "Elemental Balance"}]},
+            {"name": "Reckless Power", "skills": []},
+            {"name": "Star Pact", "skills": []}
+        ]
+    }, {
+        "class_name": "shaman",
+        "class_skills": [
+            {"name": "Brute Force", "skills": [{"name": "Feral Hunger"}, {"name": "Upheaval"}]},
+            {"name": "Devouring Swarm", "skills": []},
+            {"name": "Grasping Vines", "skills": [{"name": "Entangling Vines"}]},
+            {"name": "Primal Strike", "skills": [{"name": "Thunderous Strike"}, {"name": "Torrent"}, {"name": "Storm Surge"}]},
+            {"name": "Savagery", "skills": [{"name": "Might of the Bear"}, {"name": "Tenacity of the Boar"}, {"name": "Storm Touched"}]},
+            {"name": "Mogdrogen's Pact", "skills": [{"name": "Heart of the Wild"}, {"name": "Oak Skin"}]},
+            {"name": "Primal Bond", "skills": []},
+            {"name": "Stormcaller's Pact", "skills": []},
+            {"name": "Conjure Primal Spirit", "skills": []},
+            {"name": "Storm Totem", "skills": []},
+            {"name": "Summon Briarthorn", "skills": [{"name": "Ground Slam"}, {"name": "Emboldening Presence"}]},
+            {"name": "Wendigo Totem", "skills": [{"name": "Blood Pact"}]},
+            {"name": "Wind Devil", "skills": [{"name": "Raging Tempest"}, {"name": "Maelstrom"}]}
+        ]
+    }, {
+        "class_name": "inquisitor",
+        "class_skills": [
+            {"name": "Flames of Ignaffar", "skills": [{"name": "Intensify"}, {"name": "Infernal Purge"}, {"name": "Endless Flame"}]},
+            {"name": "Horn of Gandarr", "skills": []},
+            {"name": "Inquisitor Seal", "skills": [{"name": "Arcane Empowerment"}]},
+            {"name": "Rune of Hagarrad", "skills": [{"name": "Biting Cold"}, {"name": "Chillsurge"}]},
+            {"name": "Rune of Kalastor", "skills": [{"name": "Ignition"}]},
+            {"name": "Storm Box of Elgoloth", "skills": [{"name": "Allagast's Arcane Net"}, {"name": "Lightning Tether"}]},
+            {"name": "Word of Pain", "skills": [{"name": "Word of Agony"}, {"name": "Death Sentence"}]},
+            {"name": "Word of Renewal", "skills": [{"name": "Word of Arms"}, {"name": "Vigor"}, {"name": "Steel Resolve"}]},
+            {"name": "Artifact Handling", "skills": []},
+            {"name": "Bursting Round", "skills": []},
+            {"name": "Chilling Rounds", "skills": []},
+            {"name": "Deadly Aim", "skills": []},
+            {"name": "Ranged Expertise", "skills": []},
+            {"name": "Storm Spread", "skills": []},
+            {"name": "Aura of Censure", "skills": []},
+            {"name": "Aura of Conviction", "skills": []}
+        ]
+    }, {
+        "class_name": "necromancer",
+        "class_skills": [
+            {"name": "Bone Harvest", "skills": [{"name": "Harvester of Death"}, {"name": "Dread"}, {"name": "Soul Harvest"}]},
+            {"name": "Call of the Grave", "skills": []},
+            {"name": "Drain Essence", "skills": [{"name": "Gravechill"}, {"name": "Hungering Reach"}, {"name": "Decomposition"}]},
+            {"name": "Ill Omen", "skills": []},
+            {"name": "Mark of Torment"},
+            {"name": "Ravenous Earth", "skills": [{"name": "Decay"}, {"name": "Foul Eruption"}]},
+            {"name": "Reap Spirit", "skills": []},
+            {"name": "Siphon Souls", "skills": [{"name": "Blood Boil"}]},
+            {"name": "Necrotic Edge", "skills": []},
+            {"name": "Reaping Strike", "skills": []},
+            {"name": "Harbinger of Souls", "skills": []},
+            {"name": "Master of Death", "skills": []},
+            {"name": "Spectral Binding", "skills": [{"name": "Spectral Wrath"}]},
+            {"name": "Raise Skeletons", "skills": [{"name": "Undead Legion"}, {"name": "Will of the Crypt"}]},
+            {"name": "Reap Spirit", "skills": []},
+            {"name": "Summon Blight Fiend", "skills": [{"name": "Unstable Anomaly"}, {"name": "Rotting Fumes"}, {"name": "Blight Burst"}]}
+        ]
+    }, {
+        "class_name": "oathkeeper",
+        "class_skills": [
+            {"name": "Aegis of Menhir", "skills": [{"name": "Avenging Shield"}, {"name": "Aegis of Thorns"}, {"name": "Reprisal"}]},
+            {"name": "Ascension", "skills": [{"name": "Clarity of Purpose"}]},
+            {"name": "Eye of Reckoning", "skills": [{"name": "Soulfire"}]},
+            {"name": "Judgment", "skills": [{"name": "Crushing Verdict"}, {"name": "Heart of Wrath"}]},
+            {"name": "Righteous Fervor", "skills": [{"name": "Dreeg's Reproach"}, {"name": "Consecration"}, {"name": "Retribution"}]},
+            {"name": "Vire's Might", "skills": [{"name": "Volcanic Might"}, {"name": "Volcanic Stride"}, {"name": "Tectonic Shift"}]},
+            {"name": "Resilience", "skills": []},
+            {"name": "Safeguard", "skills": []},
+            {"name": "Shattering Smash", "skills": []},
+            {"name": "Smite", "skills": []},
+            {"name": "Divine Mandate", "skills": []},
+            {"name": "Path of the Three", "skills": []},
+            {"name": "Presence of Virtue", "skills": [{"name": "Haven"}, {"name": "Rebuke"}]},
+            {"name": "Summon Guardian of Empyrion", "skills": [{"name": "Celestial Presence"}]}
+        ]
+    }
+]
