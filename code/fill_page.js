cf71d6d6354ebfeced33d70ea3768d3af624a480
William Burdett <william.burdett1@gmail.com>
2020-12-20 17:21:13 -0500

    Add source_type select:

--- code/fill_page.js
+++ code/fill_page.js
@@ -6,7 +6,7 @@ function add_to_select(select, value){
 }
 
 function fill_values(){
-    let source_damage_type = document.getElementById("source_damage_type");
+    let source_damage_type = document.getElementById("damage_type");
     all_damage_types.forEach(damage_type => add_to_select(source_damage_type, damage_type.name));
 
     let class_1 = document.getElementById("class_1");
@@ -19,6 +19,7 @@ function add_skill_to_available_list(parent_div, skill) {
     let skill_div = document.createElement("div");
     skill_div.style.display = "inline-block";
     skill_div.style.border = "1px";
+    skill_div.style.borderColor = "black";
     let name = document.createTextNode(skill.name);
     skill_div.appendChild(name);
     let checkbox = document.createElement("input");
@@ -37,7 +38,8 @@ function create_div_for_skill(skills_class_div, skill){
     skills_class_div.appendChild(div);
 }
 
-function fill_skills_div(skills_class_div_id, class_name) {
+function fill_skills_div(skills_class_div_id, class_name_id) {
+    let class_name = document.getElementById(class_name_id).value;
     if (class_name !== ""){
         let skills_class_div = document.getElementById(skills_class_div_id);
         skills_class_div.innerHTML = "";
@@ -47,8 +49,6 @@ function fill_skills_div(skills_class_div_id, class_name) {
 }
 
 function fill_skills(){
-    let class_name_1 = document.getElementById("class_1").value;
-    let class_name_2 = document.getElementById("class_2").value;
-    fill_skills_div("skills_class_1", class_name_1);
-    fill_skills_div("skills_class_2", class_name_2);
+    fill_skills_div("skills_class_1", "class_1");
+    fill_skills_div("skills_class_2", "class_2");
 }
