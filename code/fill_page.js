d74d0d6202fdeff9b72e56bf5aabc250fc1058d6
William Burdett <william.burdett1@gmail.com>
2020-12-21 11:13:07 -0500

    Bit of cleanup

--- code/fill_page.js
+++ code/fill_page.js
@@ -1,11 +1,11 @@
-function add_to_select(select, value){
+function add_to_select(select, value) {
     let option = document.createElement("option");
     option.text = value;
     option.value = value;
     select.add(option);
 }
 
-function fill_values(){
+function fill_values() {
     let source_damage_type = document.getElementById("damage_type");
     all_damage_types.forEach(damage_type => add_to_select(source_damage_type, damage_type.name));
 
@@ -16,17 +16,17 @@ function fill_values(){
 }
 
 function add_skill_to_available_list(skill_row, skill) {
-    let img_column = document.createElement("td");
+    let name_column = document.createElement("td");
     let img = document.createElement("img");
     img.alt = skill.name;
     img.src = "images/" + skill.name.replace(/ /g, "_") + ".png";
-    img_column.appendChild(img)
-    skill_row.appendChild(img_column)
+    name_column.appendChild(img);
 
-    let name_column = document.createElement("td");
+    name_column.appendChild(document.createElement("br"));
+
     let name = document.createTextNode(skill.name);
     name_column.appendChild(name);
-    skill_row.appendChild(name_column)
+    skill_row.appendChild(name_column);
 
     let checkbox_column = document.createElement("td");
     let checkbox = document.createElement("input");
@@ -38,44 +38,44 @@ function add_skill_to_available_list(skill_row, skill) {
     skill_row.appendChild(checkbox_column)
 }
 
-function add_empty_column(skill_row){
-    let empty_column = document.createElement("td");
-    empty_column.innerHTML = "&nbsp;";
-    skill_row.appendChild(empty_column);
+function add_empty_column(skill_row) {
+    for (let i = 0; i < 2; i + {}) {
+        let empty_column = document.createElement("td");
+        empty_column.innerHTML = "&nbsp;";
+        skill_row.appendChild(empty_column);
+    }
 }
 
-function create_div_for_skill(skill_table, skill, max_dependencies){
+function create_div_for_skill(skill_table, skill, max_dependencies) {
     let skill_row = document.createElement("tr");
     let curent_count = 1;
     add_skill_to_available_list(skill_row, skill);
     curent_count += skill.skills.length;
     skill.skills.forEach(dependent_skill => add_skill_to_available_list(skill_row, dependent_skill));
-    for (let i=curent_count; i<=max_dependencies; i++){
+    for (let i = curent_count; i <= max_dependencies; i++) {
         add_empty_column(skill_row)
     }
     skill_table.appendChild(skill_row);
 }
 
-function fill_skills_div(skills_class_div_id, class_name_id) {
-    let class_name = document.getElementById(class_name_id).value;
-    let skills_class_div = document.getElementById(skills_class_div_id);
-    skills_class_div.innerHTML = "";
-    if (class_name !== ""){
-        let table = document.createElement("table");
-        table.style.fontSize = "9";
-        skills_class_div.appendChild(table);
-        let skills = get_skills_for_class(class_name);
-        let max_dependencies = 1;
-        skills.forEach(skill => {
-            if (skill.skills.length > max_dependencies){
-                max_dependencies = skill.skills.length;
-            }
-        })
-        skills.forEach(skill => create_div_for_skill(table, skill, max_dependencies));
-    }
-}
 
-function fill_skills(){
-    fill_skills_div("skills_class_1", "class_1");
-    fill_skills_div("skills_class_2", "class_2");
+function fill_skills() {
+    for (let i = 1; i <= 2; i++) {
+        let class_name = document.getElementById("class_" + i).value;
+        let skills_class_div = document.getElementById("skills_class_" + i);
+        skills_class_div.innerHTML = "";
+        if (class_name !== "") {
+            let table = document.createElement("table");
+            table.style.fontSize = "9";
+            skills_class_div.appendChild(table);
+            let skills = get_skills_for_class(class_name);
+            let max_dependencies = 1;
+            skills.forEach(skill => {
+                if (skill.skills.length > max_dependencies) {
+                    max_dependencies = skill.skills.length;
+                }
+            })
+            skills.forEach(skill => create_div_for_skill(table, skill, max_dependencies));
+        }
+    }
 }
