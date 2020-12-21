facc3749a952dd859cf932f97c6f0e48cd9e18ed
William Burdett <william.burdett1@gmail.com>
2020-12-21 10:47:14 -0500

    Switch to table based design

--- code/fill_page.js
+++ code/fill_page.js
@@ -15,31 +15,45 @@ function fill_values(){
     all_skills.forEach(obj => add_to_select(class_2, obj.class_name));
 }
 
-function add_skill_to_available_list(parent_div, skill) {
-    let skill_div = document.createElement("div");
-    skill_div.style.display = "inline-block";
-    skill_div.style.border = "1px";
-    skill_div.style.borderColor = "black";
+function add_skill_to_available_list(skill_row, skill) {
+    let img_column = document.createElement("td");
     let img = document.createElement("img");
     img.alt = skill.name;
     img.src = "images/" + skill.name.replace(/ /g, "_") + ".png";
-    skill_div.appendChild(img)
+    img_column.appendChild(img)
+    skill_row.appendChild(img_column)
+
+    let name_column = document.createElement("td");
     let name = document.createTextNode(skill.name);
-    skill_div.appendChild(name);
+    name_column.appendChild(name);
+    skill_row.appendChild(name_column)
+
+    let checkbox_column = document.createElement("td");
     let checkbox = document.createElement("input");
     checkbox.setAttribute("type", "checkbox");
     checkbox.id = skill.name;
     checkbox.name = "available_skills";
     checkbox.checked = skill.default;
-    skill_div.appendChild(checkbox);
-    parent_div.appendChild(skill_div);
+    checkbox_column.appendChild(checkbox)
+    skill_row.appendChild(checkbox_column)
 }
 
-function create_div_for_skill(skills_class_div, skill){
-    let div = document.createElement("div")
-    add_skill_to_available_list(div, skill);
-    skill.skills.forEach(dependent_skill => add_skill_to_available_list(div, dependent_skill));
-    skills_class_div.appendChild(div);
+function add_empty_column(skill_row){
+    let empty_column = document.createElement("td");
+    empty_column.innerHTML = "&nbsp;";
+    skill_row.appendChild(empty_column);
+}
+
+function create_div_for_skill(skill_table, skill, max_dependencies){
+    let skill_row = document.createElement("tr");
+    let curent_count = 1;
+    add_skill_to_available_list(skill_row, skill);
+    curent_count += skill.skills.length;
+    skill.skills.forEach(dependent_skill => add_skill_to_available_list(skill_row, dependent_skill));
+    for (let i=curent_count; i<=max_dependencies; i++){
+        add_empty_column(skill_row)
+    }
+    skill_table.appendChild(skill_row);
 }
 
 function fill_skills_div(skills_class_div_id, class_name_id) {
@@ -47,8 +61,16 @@ function fill_skills_div(skills_class_div_id, class_name_id) {
     let skills_class_div = document.getElementById(skills_class_div_id);
     skills_class_div.innerHTML = "";
     if (class_name !== ""){
+        let table = document.createElement("table");
+        skills_class_div.appendChild(table);
         let skills = get_skills_for_class(class_name);
-        skills.forEach(skill => create_div_for_skill(skills_class_div, skill));
+        let max_dependencies = 1;
+        skills.forEach(skill => {
+            if (skill.skills.length > max_dependencies){
+                max_dependencies = skill.skills.length;
+            }
+        })
+        skills.forEach(skill => create_div_for_skill(table, skill, max_dependencies));
     }
 }
 
